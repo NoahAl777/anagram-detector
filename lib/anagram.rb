39890472a08a02e3f9ab78adbf3d5e5881a83957
noah <crashlive112@gmail.com>
2020-12-05 01:32:50 -0500

    commit tree 264629735809a7cd80b389e399ef6041ae63ce9f

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(words_list)
+        words_list.select {|maybe_match| maybe_match.split("").sort == @word.split("").sort}        
+    end
+end
+        
